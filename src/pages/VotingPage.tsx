f946001aba482ca1ac6549e67e7ce328fb9ba852
faruqeclypst <faruq.blogger@gmail.com>
2024-09-24 23:35:22 +0700

    Fitur Token Guru dan Token Siswa

--- src/pages/VotingPage.tsx
+++ src/pages/VotingPage.tsx
@@ -269,7 +269,7 @@ const VotingPage: React.FC = () => {
           <motion.h1 
             initial={{ y: -50 }}
             animate={{ y: 0 }}
-            className="text-5xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600 mb-6"
+            className="text-5xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-600 mb-6"
           >
             PEMOS {new Date().getFullYear()} 
           </motion.h1>
